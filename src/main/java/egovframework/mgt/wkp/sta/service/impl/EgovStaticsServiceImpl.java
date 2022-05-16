5d05460aa5368dff8d9c39bd1278993ecfb255bf
jb.choi <jb.choi@deepfine.ai>
2022-05-16 18:37:49 +0900

    작업리스트 요청사항 13번 항목 - 목록 하단의 페이지 표시범위 확대(현재 5 → 10 이상)

--- src/main/java/egovframework/mgt/wkp/sta/service/impl/EgovStaticsServiceImpl.java
+++ src/main/java/egovframework/mgt/wkp/sta/service/impl/EgovStaticsServiceImpl.java
@@ -3,12 +3,7 @@ package egovframework.mgt.wkp.sta.service.impl;
 import egovframework.com.utl.wed.comm.ListWithPageNavigation;
 import egovframework.com.utl.wed.comm.PageNavigation;
 import egovframework.com.wkp.kno.service.KnowledgeVO;
-import egovframework.com.wkp.qna.service.ImprovementVO;
-import egovframework.mgt.wkp.sta.service.EgovStaticsService;
-import egovframework.mgt.wkp.sta.service.StaticsKnowledgeVO;
-import egovframework.mgt.wkp.sta.service.StaticsConnectVO;
-import egovframework.mgt.wkp.sta.service.StaticsQnaVO;
-import egovframework.mgt.wkp.sta.service.StaticsVO;
+import egovframework.mgt.wkp.sta.service.*;
 import egovframework.rte.fdl.cmmn.EgovAbstractServiceImpl;
 import org.springframework.stereotype.Service;
 
@@ -173,7 +168,7 @@ public class EgovStaticsServiceImpl extends EgovAbstractServiceImpl implements E
 
         ListWithPageNavigation<KnowledgeVO> result = new ListWithPageNavigation<>();
 
-        PageNavigation pageNavigation = new PageNavigation(selectKnowledgeListCount(), knowledgeVO.getPage(), null, null);
+        PageNavigation pageNavigation = new PageNavigation(selectKnowledgeListCount(), knowledgeVO.getPage(), null, 10);
         knowledgeVO.setItemCountPerPage(pageNavigation.getItemCountPerPage());
         knowledgeVO.setItemOffset(pageNavigation.getItemCountPerPage() * (knowledgeVO.getPage() - 1));
         result.setList(staticsDAO.selectKnowledgeList(knowledgeVO));
@@ -192,7 +187,7 @@ public class EgovStaticsServiceImpl extends EgovAbstractServiceImpl implements E
         
 		ListWithPageNavigation<KnowledgeVO> result = new ListWithPageNavigation<>();
 
-        PageNavigation pageNavigation = new PageNavigation(selectInterestsListCount(), knowledgeVO.getPage(), null, null);
+        PageNavigation pageNavigation = new PageNavigation(selectInterestsListCount(), knowledgeVO.getPage(), null, 10);
         knowledgeVO.setItemCountPerPage(pageNavigation.getItemCountPerPage());
         knowledgeVO.setItemOffset(pageNavigation.getItemCountPerPage() * (knowledgeVO.getPage() - 1));
         result.setList(staticsDAO.selectInterestsList(knowledgeVO));
@@ -209,7 +204,7 @@ public class EgovStaticsServiceImpl extends EgovAbstractServiceImpl implements E
     @Override
     public ListWithPageNavigation<StaticsKnowledgeVO> selectViewStatics(StaticsKnowledgeVO param) {
         ListWithPageNavigation<StaticsKnowledgeVO> listWithPageNavigation = new ListWithPageNavigation<>();
-        PageNavigation pageNavigation = new PageNavigation(selectViewStaticsCount(param), param.getPage(), null, null);
+        PageNavigation pageNavigation = new PageNavigation(selectViewStaticsCount(param), param.getPage(), null, 10);
         param.setItemCountPerPage(pageNavigation.getItemCountPerPage());
         param.setItemOffset(pageNavigation.getItemCountPerPage() * (param.getPage() - 1));
         listWithPageNavigation.setList(staticsDAO.selectViewStatics(param));
@@ -225,7 +220,7 @@ public class EgovStaticsServiceImpl extends EgovAbstractServiceImpl implements E
     @Override
     public ListWithPageNavigation<StaticsKnowledgeVO> selectRecommendStatics(StaticsKnowledgeVO param) {
         ListWithPageNavigation<StaticsKnowledgeVO> listWithPageNavigation = new ListWithPageNavigation<>();
-        PageNavigation pageNavigation = new PageNavigation(selectRecommendStaticsCount(param), param.getPage(), null, null);
+        PageNavigation pageNavigation = new PageNavigation(selectRecommendStaticsCount(param), param.getPage(), null, 10);
         param.setItemCountPerPage(pageNavigation.getItemCountPerPage());
         param.setItemOffset(pageNavigation.getItemCountPerPage() * (param.getPage() - 1));
         listWithPageNavigation.setList(staticsDAO.selectRecommendStatics(param));
@@ -241,7 +236,7 @@ public class EgovStaticsServiceImpl extends EgovAbstractServiceImpl implements E
     @Override
     public ListWithPageNavigation<StaticsKnowledgeVO> selectUserStatics(StaticsKnowledgeVO param) {
         ListWithPageNavigation<StaticsKnowledgeVO> listWithPageNavigation = new ListWithPageNavigation<>();
-        PageNavigation pageNavigation = new PageNavigation(selectUserStaticsCount(param), param.getPage(), null, null);
+        PageNavigation pageNavigation = new PageNavigation(selectUserStaticsCount(param), param.getPage(), null, 10);
         param.setItemCountPerPage(pageNavigation.getItemCountPerPage());
         param.setItemOffset(pageNavigation.getItemCountPerPage() * (param.getPage() - 1));
         listWithPageNavigation.setList(staticsDAO.selectUserStatics(param));
@@ -257,7 +252,7 @@ public class EgovStaticsServiceImpl extends EgovAbstractServiceImpl implements E
     @Override
     public ListWithPageNavigation<StaticsKnowledgeVO> selectRecommendUserStatics(StaticsKnowledgeVO param) {
         ListWithPageNavigation<StaticsKnowledgeVO> listWithPageNavigation = new ListWithPageNavigation<>();
-        PageNavigation pageNavigation = new PageNavigation(selectRecommendUserStaticsCount(param), param.getPage(), null, null);
+        PageNavigation pageNavigation = new PageNavigation(selectRecommendUserStaticsCount(param), param.getPage(), null, 10);
         param.setItemCountPerPage(pageNavigation.getItemCountPerPage());
         param.setItemOffset(pageNavigation.getItemCountPerPage() * (param.getPage() - 1));
         listWithPageNavigation.setList(staticsDAO.selectRecommendUserStatics(param));
@@ -273,7 +268,7 @@ public class EgovStaticsServiceImpl extends EgovAbstractServiceImpl implements E
     @Override
     public ListWithPageNavigation<StaticsKnowledgeVO> selectOrgStatics(StaticsKnowledgeVO param) {
         ListWithPageNavigation<StaticsKnowledgeVO> listWithPageNavigation = new ListWithPageNavigation<>();
-        PageNavigation pageNavigation = new PageNavigation(selectOrgStaticsCount(param), param.getPage(), null, null);
+        PageNavigation pageNavigation = new PageNavigation(selectOrgStaticsCount(param), param.getPage(), null, 10);
         param.setItemCountPerPage(pageNavigation.getItemCountPerPage());
         param.setItemOffset(pageNavigation.getItemCountPerPage() * (param.getPage() - 1));
         listWithPageNavigation.setList(staticsDAO.selectOrgStatics(param));
